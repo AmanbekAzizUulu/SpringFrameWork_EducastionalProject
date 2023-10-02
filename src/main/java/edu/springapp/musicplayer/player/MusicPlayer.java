08ee14d1bc8a25e557d047aeb878695fc0fc4219
Amanbek Aziz Uulu <amanbekazizuulu@gmail.com>
2023-10-02 15:35:57 +0600

    Dependency injection via setter method

--- src/main/java/edu/springapp/musicplayer/player/MusicPlayer.java
+++ src/main/java/edu/springapp/musicplayer/player/MusicPlayer.java
@@ -1,65 +1,41 @@
 
 package edu.springapp.musicplayer.player;
 
-
 import java.util.List;
-
 
 import edu.springapp.musicplayer.playlist.Music;
 
-
 public class MusicPlayer {
-	private Music music;
-	private List<Music> playList;
-	private int indexOfMusic = 0;
-	private int countOfPressesOfPlayButton = 0;
+    private Music music;
+    private List<Music> playList;
 
-	public MusicPlayer (Music music) {
-		this.music = music;
-		playList.add (music);
-	}
+    public MusicPlayer() {
+    }
 
+    public MusicPlayer(Music music) {
+	this.music = music;
+	playList.add(music);
+    }
+    
+    
+    public void play() {
+	System.out.println("Plays:\t" + this.music.getSong());
+    }
 
-	public void playMusic () {
-		countOfPressesOfPlayButton ++;
-		System.out.println ("Plays: " + this.music.getSong ());
-	}
+    public List<Music> getPlayList() {
+	return playList;
+    }
 
+    public void setPlayList(List<Music> playList) {
+	this.playList = playList;
+    }
 
-	public void next () {
-		if (countOfPressesOfPlayButton == 1) {
-			indexOfMusic = 1;
-		}
-		this.music = playList.get (indexOfMusic ++);
-		if (indexOfMusic == playList.size ()) {
-			indexOfMusic = 0;
-		}
-		this.playMusic ();
-	}
+    public Music getMusic() {
+	return music;
+    }
 
-
-	public List<Music> getPlayList () {
-		return playList;
-	}
-
-
-	public void setPlayList (List<Music> playList) {
-		this.playList = playList;
-	}
-
-
-	public void addMusicToPlaylist (Music music) {
-		this.playList.add (music);
-	}
-
-
-	public Music getMusic () {
-		return music;
-	}
-
-
-	public void setMusic (Music music) {
-		this.music = music;
-	}
+    public void setMusic(Music music) {
+	this.music = music;
+    }
 
 }
